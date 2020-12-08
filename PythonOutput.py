f5147ad3c5c6efb4e07259d72632efc62f395617
ryandecosmo <ryandecosmo>
2020-12-08 00:34:02 +0000

    more files
    and lib

--- PythonOutput.py
+++ PythonOutput.py
@@ -1,5 +1,22 @@
 # Databricks notebook source
-spark
+import Funk
+
+# COMMAND ----------
+
+import sys
+print(sys.path)
+
+# COMMAND ----------
+
+
+sys.path.append("https://demo.cloud.databricks.com/#folder/8947539/")
+
+
+
+
+# COMMAND ----------
+
+import module.Funk
 
 # COMMAND ----------
 
---
+++ module/Funk.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+def successful():
+  print("success!")
+  return
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
