11e478e3d056c4a2bf17d8f743b55dacca6dc1e1
ryandecosmo <ryandecosmo>
2020-12-08 00:35:16 +0000

    removed cell
    r

--- PythonOutput.py
+++ PythonOutput.py
@@ -8,14 +8,6 @@ print(sys.path)
 
 # COMMAND ----------
 
-
-sys.path.append("https://demo.cloud.databricks.com/#folder/8947539/")
-
-
-
-
-# COMMAND ----------
-
 import module.Funk
 
 # COMMAND ----------
